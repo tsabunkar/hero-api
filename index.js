6311d57fc9644e0925a45c41facb5a1f81b75e12
tsabunkar@gmail.com <tsabunkar@gmail.com>
2019-03-21 16:25:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) =>
+    res.json({
+        message: 'hi bro'
+    })
+)
+app.get('/hero', (req, res) =>
+    res.json({
+        message: 'hi Hero How are you ?'
+    })
+)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
